f65dcf6eab0e719006577a5bf6b11c2ff3d66215
BrentVanant1999 <bvancar@yahoo.com>
2018-08-21 00:32:37 -0500

    Adding in material design principles.

---
+++ public/js/js/buttonInit.js
@@ -0,0 +1,176 @@
+var createModal = document.getElementById('eventCreateModal');
+var inModal = false;
+var createMarkerExists = false;
+/*
+ * Event type card selection handling
+ */
+$('.formCard').on('click', function(e) {
+  e.preventDefault();
+  $('.formCard').removeClass('active');
+  $(this).addClass('active');
+});
+
+/*
+ * Modal Submission Handling
+ */
+$('#eventInputButton').on('click', function(e) {
+  e.preventDefault();
+  return false;
+});
+/*
+ *  Slider management
+ */
+ var durationSlider = document.getElementById("eventDurationInput");
+ var displaySlider = document.getElementById("displayDuration");
+ var minutesInput = durationSlider.value;
+ var hoursDisplay = Math.floor(minutesInput/60);
+ var minutesDisplay = (minutesInput%60);
+ if (hoursDisplay > 0){
+   if (minutesDisplay != 0) {
+     if (hoursDisplay < 2) {
+       displaySlider.innerHTML = "("+ hoursDisplay+" hour and " + minutesDisplay +" minutes."+")";
+     }
+     else {
+       displaySlider.innerHTML = "("+hoursDisplay+" hours and " + minutesDisplay +" minutes."+")";
+     }
+   }
+   else {
+     if (hoursDisplay < 2) {
+       displaySlider.innerHTML = "("+hoursDisplay+" hour."+")";
+     }
+     else {
+       displaySlider.innerHTML = "("+hoursDisplay+" hours."+")";
+     }
+   }
+ }
+ else {
+   displaySlider.innerHTML = "("+minutesDisplay+" minutes."+")";
+ }
+ // Update the current slider value (each time you drag the slider handle)
+ durationSlider.oninput = function() {
+   minutesInput = durationSlider.value;
+   hoursDisplay = Math.floor(minutesInput/60);
+   minutesDisplay = (minutesInput%60);
+   if (hoursDisplay > 0){
+     if (minutesDisplay != 0) {
+       if (hoursDisplay < 2) {
+         displaySlider.innerHTML = "("+ hoursDisplay+" hour and " + minutesDisplay +" minutes."+")";
+       }
+       else {
+         displaySlider.innerHTML = "("+hoursDisplay+" hours and " + minutesDisplay +" minutes."+")";
+       }
+     }
+     else {
+       if (hoursDisplay < 2) {
+         displaySlider.innerHTML = "("+hoursDisplay+" hour."+")";
+       }
+       else {
+         displaySlider.innerHTML = "("+hoursDisplay+" hours."+")";
+       }
+     }
+   }
+   else {
+     displaySlider.innerHTML = "("+minutesDisplay+" minutes."+")";
+   }
+
+ }
+
+/*
+ * Give the locator button the property:
+ * when clicked - call funtion locateClientUser()
+ */
+$('#locator').on('click', function(){
+  locateClientUser();
+});
+
+/*
+ * Give the eventCreateButton  the property:
+ * when clicked while in the event creation modal do nothing
+ * when not in the modal check to see if a location has been locationfound
+ * and if so show the modal. If not prompt user to setup a location.
+ */
+$('#eventCreateButton').on('click', function(){
+  if (inModal === false) {
+    if (createMarkerExists === false) {
+      createNewEventMarker();
+      toggleButtons();
+    }
+    else {
+      console.log("Fired");
+      createModal.style.display="block";
+      var latLng = markerUI.getLatLng();
+      tempLng = latLng.lng;
+      tempLat = latLng.lat;
+      inModal = true;
+      toggleButtons();
+    }
+  }
+});
+
+/*
+ * Give the eventCreateButton  the property:
+ * when clicked remove the ui layer and exit the event creation process.
+ */
+$('#cancelEventCreation').on('click', function(){
+  mymap.removeLayer(markerGroupUI);
+  hasLocation=false;
+  inModal=false;
+});
+
+/*
+ * Give the eventCreateButton the property:
+ * when clicked check if the form is correct and if so remove the ui marker and
+ * call writeUserEvent to write the data to firebase.
+ */
+$('#submitEvent').on('click', function(){
+  if (validateEventInput()) {
+    mymap.removeLayer(markerGroupUI);
+    writeUserEvent();
+    hasLocation=false;
+    inModal=false;
+  }
+});
+
+/*
+ * Give the eventCreateButton the property:
+ * when clicked call the cancel button function
+ */
+$('#eventCancelButton').on('click', function(){
+  cancelButton();
+});
+
+/*
+ * Change the buttons display back to the normal display and remove the ui
+ * marker
+ */
+function cancelButton(){
+  toggleButtons();
+  mymap.removeLayer(markerGroupUI);
+  inModal = false;
+  hasLocation = false;
+}
+
+/*
+ * function to toggle buttons
+ * works by removing styles and adding styles to make the buttons match whatever
+ * functionality they are supposed to have at any given time.
+ */
+function toggleButtons(){
+    if (createMarkerExists === false){
+      document.getElementById("eventCreateButtonIcon").classList.remove('fa-plus');
+      document.getElementById("eventCreateButtonIcon").classList.add('fa-check');
+      document.getElementById("eventCancelButton").classList.remove('invis');
+      document.getElementById("eventCancelButton").classList.add('vis');
+      createMarkerExists = true;
+    }
+    else {
+      document.getElementById("eventCreateButtonIcon").classList.remove('fa-check');
+      document.getElementById("eventCreateButtonIcon").classList.add('fa-plus');
+      document.getElementById("eventCancelButton").classList.remove('vis');
+      document.getElementById("eventCancelButton").classList.add('invis');
+      createMarkerExists = false;
+    }
+}
+/*
+ * handle click out of modal
+ */
---
+++ public/js/js/main.js
@@ -0,0 +1,242 @@
+/*
+ * Constant Declerations
+ */
+var eventsLocationRef = firebase.database().ref('eventsLocations');
+var geoFire;
+var searchRadius = 2;
+
+/*
+ * Creates the marker that users use to plan where there event will take place
+ */
+function createNewEventMarker() {
+  markerGroupUI = L.layerGroup().addTo(mymap);
+  markerUI = L.marker(mymap.getCenter(), {
+      draggable:true,
+      icon:creatingIcon
+  }).addTo(markerGroupUI);
+      //handle drag events
+      markerUI.on('dragend', function(event){
+      var userCreateEventMarker = event.target;
+  });
+}
+
+
+/*
+ * Handles the user state
+ * Writes user data to firebase
+ * Starts querrying the database
+ */
+initApp = function() {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+       var displayName = user.displayName;
+       var email = user.email;
+       var uid = user.uid;
+       firebase.database();
+       eventsLocationRef = firebase.database().ref('eventsLocations');
+       geoFire = new GeoFire(eventsLocationRef);
+       writeUserData(uid, email, displayName);
+       startDatabaseQuery();
+    }
+  }, function(error) {
+    //TODO figure out what to do with errors... how to display to users?
+    console.log(error);
+  });
+};
+
+/*
+ * Queries firebase for event info
+ * Currently it loads all events into this (clientside application)
+ * This will scale horribly if there are a ton of events and thus needs to be
+ * fixed. TODO figure out a way to load events in chunks based off location
+ */
+function startDatabaseQuery() {
+
+  var geoQuery = geoFire.query({
+    center: [curLat, curLng],
+    radius: searchRadius
+  });
+  var onKeyEnteredRegistration = geoQuery.on("key_entered", function(key, location) {
+    var eventRef = firebase.database().ref('events/' + key);
+      eventRef.on('value', function(snapshot) {
+        var title = snapshot.val().title;
+        var org = snapshot.val().org;
+        var type = snapshot.val().type;
+        var endTime = snapshot.val().endTime;
+        var lat = location[0];
+        var lng =  location[1];
+        var description = snapshot.val().description;
+        createEventMarker(lat,lng,title,org,type,endTime,description);
+      });
+  });
+  var onReadyRegistration = geoQuery.on("ready", function() {
+    geoQuery.cancel();
+  });
+}
+
+/*
+ * Creates a marker that stores value about an event.
+ */
+function createEventMarker(lat, lng, titleIn, orgIn, typeIn, timeIn, descriptIn){
+  //create marker reference
+  var newMarker;
+  /*switch statement to handle what type of event is created.
+   *super inefficient but i couldn;t get logic to work within the actual
+   *marker creation
+   */
+  switch(typeIn){
+    case(1):
+    {
+      //create the marker object and assign reference to it
+      newMarker = new eventMarker([lat, lng],{
+        clickable: true,
+        icon: socialIcon
+      });
+      newMarker.bindPopup("<b>"+titleIn+"</b><br>"+descriptIn);
+      newMarker.addTo(socialEvents);
+    }
+    break;
+    case(2):
+    {
+      newMarker = new eventMarker([lat, lng],{
+        clickable: true,
+        icon: sportingIcon
+      });
+  newMarker.bindPopup("<b>"+titleIn+"</b><br>"+descriptIn);
+      newMarker.addTo(sportingEvents);
+    }
+    break;
+    case(3):
+    {
+      newMarker = new eventMarker([lat, lng],{
+        clickable: true,
+        icon: academicIcon
+      });
+        newMarker.bindPopup("<b>"+titleIn+"</b><br>"+descriptIn);
+      newMarker.addTo(academicEvents);
+    }
+    break;
+    case(4):
+    {
+      newMarker = new eventMarker([lat, lng],{
+        clickable: true,
+        icon: marketIcon
+      });
+      newMarker.bindPopup("<b>"+titleIn+"</b><br>"+descriptIn);
+      newMarker.addTo(marketEvents);
+    }
+    break;
+    case(5):
+    {
+      newMarker = new eventMarker([lat, lng],{
+        clickable: true,
+        icon: techIcon
+      });
+      newMarker.bindPopup("<b>"+titleIn+"</b><br>"+descriptIn);
+      newMarker.addTo(techEvents);
+    }
+    break;
+    case(6):
+    {
+      newMarker = new eventMarker([lat, lng],{
+        clickable: true,
+        icon: healthIcon
+    });
+      newMarker.bindPopup("<b>"+titleIn+"</b><br>"+descriptIn);
+      newMarker.addTo(healthEvents);
+    }
+    break;
+  }
+  //adds the marker to the main marker group created at the beggining of this file
+
+}
+
+/*
+ * Checks html elements in the event creation modal. Sends out an alert if
+ * required inputs have not yet been input.
+ * returns true if everything has an input.
+ */
+function validateEventInput() {
+  var titleInput = document.getElementById("eventNameInput");
+  var orgInput = document.getElementById("eventOrganizationInput");
+  var timeInput = document.getElementById("eventDurationInput");
+  var descriptionInput = document.getElementById("eventDescriptionInput");
+
+  if (titleInput.value === ""){
+    alert("Event title Needed.");
+    return false;
+  }
+  else if (orgInput.value === ""){
+    alert("Event organization needed.");
+    return false;
+  }
+  else if (timeInput.value === ""){
+    alert("End time needed");
+    return false;
+  }
+  return true;
+}
+
+/*
+ * write to firebase a user created event.
+ * creates local references to the database and the input elements
+ * uses these references to push data to the events collection in firebase
+ */
+function writeUserEvent() {
+  var myUserName = firebase.auth().currentUser.displayName;
+  var eventsRef = firebase.database().ref('events');
+  var titleInput = document.getElementById("eventNameInput");
+  var orgInput = document.getElementById("eventOrganizationInput");
+  var typeInput = document.getElementById("typeEventInput") ; //TODO FIX TYPE OF INPUT
+  var timeInput = document.getElementById("eventDurationInput"); //TODO FIGURE OUT TIME INPUT
+  var descriptionInput = document.getElementById("eventDescriptionInput");
+  //one api call
+  var eventID = eventsRef.push ({
+   title: titleInput.value,
+   owner: myUserName,
+   description: descriptionInput.value,
+   endTime: timeInput,
+   type: parseInt(typeInput.value),
+   org: orgInput.value
+ }).getKey();
+  //one api call
+  geoFire.set(eventID, [tempLat, tempLng]).then(function() {
+  console.log("Provided key has been added to GeoFire");
+    }, function(error) {
+  console.log("Error: " + error);
+  });
+}
+
+
+/*
+ * write to firebase user data passed in as arguments.
+ */
+function writeUserData(userId, email, displayName ) {
+  firebase.database().ref('users/' + userId).set({
+    username: displayName,
+    email: email
+  });
+}
+/*
+ *  return a date object which represents an end time of an event
+ */
+function getEndTime(minuteDuration) {
+    var dateNow = new Date();
+    var utc = dataNow.getTime();
+    var endTime = moment(utc).add(minuteDuration, 'm').toDate();
+    return endTime;
+}
+/*
+ *  return a date object which represents the current time of an event
+ */
+function calcCurrentTime() {
+    var dateNow = new Date();
+    var utc = dateNow.getTime();
+    return utc;
+}
+/*
+ * When the page is loaded, call initApp
+ */
+window.addEventListener('load', function() {
+  initApp();
+});
